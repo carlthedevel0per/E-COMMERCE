fbf487cff01c7c428daef9df19390c8c8408d202
ishowcarl <imontemayor285@gmail.com>
2026-08-04 07:47:51 +0800

    used the js file for products

--- scripts/products.js
+++ scripts/products.js
@@ -1,37 +1,3 @@
-const products = [
-  {
-    name: "SMG",
-    image: "Photos/smg.png",
-    priceCents: 120025
-  },
-  {
-    name: "ANACONDA",
-    image: "Photos/anaconda.png",
-    priceCents: 80075
-  },
-  {
-    name: "AK-47",
-    image: "Photos/ak47.png",
-    priceCents: 150090
-  },
-  {
-    name: "SHOTGUN",
-    image: "Photos/shotgun.png",
-    priceCents: 200030
-  },
-  {
-    name: "SNIPER",
-    image: "Photos/sniper.png",
-    priceCents: 400050
-  },
-  {
-    name: "PISTOL",
-    image: "Photos/pistol.png",
-    priceCents: 100060
-  }
-
-];
-
 let productsHTML = '';
 
 products.forEach((product) => {
